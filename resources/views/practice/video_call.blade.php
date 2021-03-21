982a7ce0b04822ccf3bc5f2ad8dfa6d6342d2db9
ryuimoto <>
2021-03-21 20:13:42 +0900

    test add

--- resources/views/practice/video_call.blade.php
+++ resources/views/practice/video_call.blade.php
@@ -7,6 +7,22 @@
     <title>{{ config('title.main_title') }}/ビデオ通話</title>
 </head>
 <body>
-    
+    <video id="myVideo" width="400" height="300" autoplay="1" src=""></video>
+
+    <script type="text/javascript">
+        navigator.getUserMedia = navigator.getUserMedia || navigator.webkitGetUserMedia
+        window.URL = window.URL || window.webkitURL;
+
+        let video = document.getElementById('myVideo');
+        let localStream = null;
+        navigator.getUserMedia({video: true, audio:false},
+            function(stream){
+                console.log(stream);
+                video.srcObject = stream;
+            },function(err){
+                console.log(err);
+            }
+        );
+    </script>
 </body>
 </html>
